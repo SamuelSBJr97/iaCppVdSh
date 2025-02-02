a9e717dbecb258dfa1b35ab9c7fdc56e95c38bcb
samuelsilvabezerrajunior <samuelsilvabezerrajunior@gmail.com>
2025-02-02 17:50:09 -0300

    update

--- assets/linux.sh
+++ assets/linux.sh
@@ -3,16 +3,15 @@
 sudo apt update
 sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
 
-cd ~
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
 
-cd ~/opencv
+cd /content/opencv
 mkdir build
-cd build
+cd /content/opencv/build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+      -D OPENCV_EXTRA_MODULES_PATH=/content/opencv_contrib/modules \
       -D WITH_CUDA=ON \
       -D ENABLE_FAST_MATH=1 \
       -D CUDA_FAST_MATH=1 \
@@ -25,5 +24,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 make -j$(nproc)
 sudo make install
 sudo ldconfig
+
+cd /content
 
 g++ -o iaCppRemaster iaCppRemaster.cpp `pkg-config --cflags --libs opencv4`
