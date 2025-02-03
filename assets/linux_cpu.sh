50c6929aa783c1f75e0f57f51051a5c786cd6daa
Samuel da Silva B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-02-03 01:14:12 -0300

    Atualizar o linux_cpu.sh

--- assets/linux_cpu.sh
+++ assets/linux_cpu.sh
@@ -5,7 +5,19 @@ sudo apt update
 sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
 
 # Instalar o OpenCV diretamente dos repositórios do Ubuntu
-sudo apt install -y libopencv-dev python3-opencv
+sudo apt install -y libopencv-dev python3-opencv libomp-dev
+
+echo "Baixando e instalando OpenVINO..."
+OPENVINO_VERSION="2024.0.0.16972"
+OPENVINO_FILE="l_openvino_toolkit_${OPENVINO_VERSION}_x86_64.tgz"
+OPENVINO_URL="https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.0/linux/$OPENVINO_FILE"
+
+echo "Configurando OpenVINO..."
+echo "source /opt/intel/openvino_2024/setupvars.sh" >> ~/.bashrc
+source /opt/intel/openvino_2024/setupvars.sh
+
+echo "Limpeza de arquivos temporários..."
+rm -rf "$OPENVINO_FILE" "l_openvino_toolkit_${OPENVINO_VERSION}_x86_64"
 
 g++ -o /content/iaCppRemaster/iaCppRemaster /content/iaCppRemaster/src/iaCppRemaster.cpp `pkg-config --cflags --libs opencv4`
 
