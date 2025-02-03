a193e3b6d5837596d5410afc073484a35e18c366
Samuel da Silva B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-02-03 01:31:43 -0300

    Atualizar o linux_cpu.sh

--- assets/linux_cpu.sh
+++ assets/linux_cpu.sh
@@ -7,12 +7,40 @@ sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec
 # Instalar o OpenCV diretamente dos repositórios do Ubuntu
 sudo apt install -y libopencv-dev python3-opencv libomp-dev
 
-pip install openvino-colab
+# Instalando dependências do OpenVINO
+echo "Instalando dependências..."
+sudo apt install -y lsb-release wget sudo cmake build-essential
+sudo apt install -y python3 python3-pip python3-dev
+sudo apt install -y libopencv-dev libprotobuf-dev protobuf-compiler
+sudo apt install -y libssl-dev
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y git
 
-python3 << EOF
-import openvino_colab
-openvino_colab.install_openvino()
-EOF
+# Baixando o OpenVINO
+echo "Baixando o OpenVINO..."
+OPENVINO_VERSION="2024.0.0.16972"
+OPENVINO_FILE="l_openvino_toolkit_${OPENVINO_VERSION}_x86_64.tgz"
+OPENVINO_URL="https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.0/linux/$OPENVINO_FILE"
+
+wget -O "$OPENVINO_FILE" "$OPENVINO_URL"
+
+# Extraindo o arquivo
+tar -xvzf "$OPENVINO_FILE"
+
+# Instalando OpenVINO
+cd "l_openvino_toolkit_${OPENVINO_VERSION}_x86_64"
+sudo ./install.sh
+
+# Configurando variáveis de ambiente do OpenVINO
+echo "Configurando OpenVINO..."
+echo "source /opt/intel/openvino_2024/setupvars.sh" >> ~/.bashrc
+source /opt/intel/openvino_2024/setupvars.sh
+
+# Limpando arquivos temporários
+cd ..
+rm -rf "$OPENVINO_FILE" "l_openvino_toolkit_${OPENVINO_VERSION}_x86_64"
+
+echo "Instalação do OpenVINO concluída com sucesso!"
 
 g++ -o /content/iaCppRemaster/iaCppRemaster /content/iaCppRemaster/src/iaCppRemaster.cpp `pkg-config --cflags --libs opencv4`
 
