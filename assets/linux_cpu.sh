3d27b5d117d3fc7e0707f96e96184d201d204d14
Samuel da Silva B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-02-03 01:55:09 -0300

    Atualizar o linux_cpu.sh

--- assets/linux_cpu.sh
+++ assets/linux_cpu.sh
@@ -2,7 +2,7 @@
 set -e
 # Atualizar e instalar dependências básicas
 sudo apt update
-sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbb2 libtbb-dev
 
 # Instalar o OpenCV diretamente dos repositórios do Ubuntu
 sudo apt install -y libopencv-dev python3-opencv libomp-dev
