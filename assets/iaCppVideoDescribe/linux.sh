622e6dec5865454a1cd0f3fbbe5baa7edaf9844d
samuelsilvabezerrajunior <samuelsilvabezerrajunior@gmail.com>
2025-02-04 12:21:14 -0300

    update

--- assets/iaCppVideoDescribe/linux.sh
+++ assets/iaCppVideoDescribe/linux.sh
@@ -57,10 +57,8 @@ find_package(Torch REQUIRED)
 target_link_libraries(iaCppVideoDescribe ${OpenCV_LIBS} "${LIBTORCH_DIR}/lib/")
 
 # Exportar variáveis de ambiente para o runtime
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath,${TORCH_INSTALL_PREFIX}/lib")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath,${LIBTORCH_DIR}/lib")
 EOF
-
-echo "OpenCV_LIBS: ${OpenCV_LIBS} CMAKE_EXE_LINKER_FLAGS: ${CMAKE_EXE_LINKER_FLAGS} TORCH_INSTALL_PREFIX: ${TORCH_INSTALL_PREFIX}"
 
 # Configurar variáveis de ambiente para Libtorch
 export CMAKE_PREFIX_PATH=$LIBTORCH_DIR
