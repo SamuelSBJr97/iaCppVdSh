d19f62b2002e0cdba7edf071fc5c21231e363b51
samuelsilvabezerrajunior <samuelsilvabezerrajunior@gmail.com>
2025-02-04 12:58:11 -0300

    update

--- assets/iaCppVideoDescribe/linux.sh
+++ assets/iaCppVideoDescribe/linux.sh
@@ -56,12 +56,15 @@ endif()
 
 include_directories(
     \${OpenCV_INCLUDE_DIRS}
-    \${Torch_INCLUDE_DIRS}
+    \${TORCH_INCLUDE_DIRS}
+    ${LIBTORCH_DIR}/include
+    ${LIBTORCH_DIR}/include/torch/csrc/api/include
+    ${LIBTORCH_DIR}/include/torch
 )
 
 add_executable(iaCppVideoDescribe iaCppVdSh/src/iaCppVideoDescribe.cpp)
 
-target_link_libraries(iaCppVideoDescribe \${OpenCV_LIBS} \${TORCH_LIBRARIES})
+target_link_libraries(iaCppVideoDescribe \${OpenCV_LIBS} \${TORCH_LIBRARIES} c10)
 
 set(CMAKE_EXE_LINKER_FLAGS "\${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath,\${TORCH_INSTALL_PREFIX}/lib")
 EOF
@@ -71,7 +74,7 @@ export CMAKE_PREFIX_PATH=$LIBTORCH_DIR
 echo "Compilando o código..."
 mkdir -p build && cd build
 cmake -DCMAKE_PREFIX_PATH=$LIBTORCH_DIR ..
-make
+make -j$(nproc)
 
 echo "Compilação concluída com sucesso!"
 
